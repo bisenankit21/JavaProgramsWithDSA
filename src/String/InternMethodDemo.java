e307d4dd67a487947c97d71b2752b7ba08445d46
ankitbisen <bisenankit21@gmail.com>
2024-12-28 00:34:11 +0530

    Added classes.

---
+++ src/String/InternMethodDemo.java
@@ -0,0 +1,16 @@
+package String;
+
+public class InternMethodDemo {
+    public static void main(String[] args) {
+        String s1 = "Hello World!";
+        String s2 = new String("Hello World!").intern();
+        System.out.println(s1 == s2); //print true
+
+        String s3 = "Hello";
+        String s4 = new String("Hello");
+        System.out.println(s3 == s4); //print false
+
+        String s5 = s4.intern();
+        System.out.println(s5 == s3); //print true
+    }
+}
---
+++ src/String/StringPoolDemo.java
@@ -0,0 +1,24 @@
+package String;
+
+public class StringPoolDemo {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = "Hey";
+        System.out.println(s1 == s2);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
+        System.out.println(s1 == s3);
+
+        String s4 = new String("Hello");
+        String s5 = new String("Hello");
+        String s6 = new String("Hey");
+        System.out.println(s4 == s5);
+        System.out.println(s4.hashCode());
+        System.out.println(s5.hashCode());
+        System.out.println(s6.hashCode());
+        System.out.println(s4 == s5);
+        
+    }
+}
